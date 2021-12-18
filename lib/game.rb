262a5f8dc7d0e84d143b93521f020597e84e4eea
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2021-12-17 20:01:02 -0600

    Add Game class. Initialize with 3 args passed in

--- lib/game.rb
+++ lib/game.rb
@@ -1,6 +1,14 @@
 class Game
+  attr_reader :name, :cost, :national_drawing
 
-  def initialize
+  def initialize(name, cost, national_drawing)
+    @name = name
+    @cost = cost
+    @national_drawing = national_drawing
   end
+
+  def national_drawing?
+    @national_drawing
+  end 
 
 end
--- test/game_spec.rb
+++ test/game_spec.rb
@@ -4,6 +4,16 @@ require './lib/game'
 
 RSpec.describe Game do
   it 'exists' do
+    mega_millions = Game.new('Mega Millions', 5, true)
 
+    expect(mega_millions).to be_instance_of Game
+  end
+
+  it 'initializes with a name, cost to play, and whether the drawing is national' do
+    mega_millions = Game.new('Mega Millions', 5, true)
+
+    expect(mega_millions.name).to eq("Mega Millions")
+    expect(mega_millions.cost).to eq(5)
+    expect(mega_millions.national_drawing?).to be true
   end
 end
