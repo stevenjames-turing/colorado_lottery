d03a4e6471da94d30d3092dea51dcff850a3b585
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2021-12-17 20:26:22 -0600

    Add #add_game_interest(game) to add interested games to Contestant array

--- lib/contestant.rb
+++ lib/contestant.rb
@@ -1,11 +1,21 @@
 class Contestant
-  attr_reader :full_name, :age, :state_of_residence, :spending_money
+  attr_reader :full_name, :age, :state_of_residence, :spending_money,
+              :game_interests
 
   def initialize(data)
     @full_name = "#{data[:first_name]} #{data[:last_name]}"
     @age = data[:age]
     @state_of_residence = data[:state_of_residence]
     @spending_money = data[:spending_money]
+    @game_interests = []
+  end
+
+  def out_of_state?
+    @state_of_residence != "CO"
+  end
+
+  def add_game_interest(game)
+    @game_interests << game 
   end
 
 end
--- test/contestant_test.rb
+++ test/contestant_test.rb
@@ -2,28 +2,36 @@ require './lib/contestant'
 
 
 RSpec.describe Contestant do
-  it 'exists' do
-    alexander = Contestant.new({
-      first_name: 'Alexander',
-      last_name: 'Aigiades',
-      age: 28,
-      state_of_residence: 'CO',
-      spending_money: 10})
+  let (:alexander) {Contestant.new({
+    first_name: 'Alexander',
+    last_name: 'Aigiades',
+    age: 28,
+    state_of_residence: 'CO',
+    spending_money: 10})}
 
+  it 'exists' do
     expect(alexander).to be_instance_of Contestant
   end
 
   it 'initializes with a hash of data containing name, age, residency, and spending money' do
-    alexander = Contestant.new({
-      first_name: 'Alexander',
-      last_name: 'Aigiades',
-      age: 28,
-      state_of_residence: 'CO',
-      spending_money: 10})
-
     expect(alexander.full_name).to eq("Alexander Aigiades")
     expect(alexander.age).to eq(28)
     expect(alexander.state_of_residence).to eq("CO")
     expect(alexander.spending_money).to eq(10)
+  end
+
+  it 'knows whether the Contestant is out of state' do
+    expect(alexander.out_of_state?).to be false
+  end
+
+  it 'initializes without any game interests' do
+    expect(alexander.game_interests).to eq([])
+  end
+
+  it 'can add interested games' do
+    alexander.add_game_interest('Mega Millions')
+    alexander.add_game_interest('Pick 4')
+
+    expect(alexander.game_interests).to eq(["Mega Millions", "Pick 4"])
   end
 end
